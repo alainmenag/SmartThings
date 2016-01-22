b31ac27eb0596c157637e4259fde91cc4c3dafa4
Alain Mena Galindo <alainmenag@gmail.com>
2016-01-22 14:13:25 -0800

    Making changes.

--- smartapps/wimzel/living-lights.src/living-lights.groovy
+++ smartapps/wimzel/living-lights.src/living-lights.groovy
@@ -1,7 +1,7 @@
 /**
  *  Living Lights
  *
- *  Copyright 2015 Alain Mena Galindo
+ *  Copyright 2016 Alain Mena Galindo
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
